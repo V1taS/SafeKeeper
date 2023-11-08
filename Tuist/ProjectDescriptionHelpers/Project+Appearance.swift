c3e8e2de136ff525965f852ce07cf3f1398df73e
V1taS <SosinVitalii@iCloud.com>
2023-11-08 19:38:32 +0100

    initial commit

---
+++ Tuist/ProjectDescriptionHelpers/Project+Appearance.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+// MARK: - Public property
+
+// Сборке для App store
+public let marketingVersion = "1.0"
+// С каждой сборкой увеличиваем на 1
+public let currentProjectVersion = "1"
+
+// App
+public let organizationName = "SosinVitalii.com"
+public let reverseOrganizationName = "com.sosinvitalii"
+public let appName = "SafeKeeper"
+public let displayName = "Safe Keeper"
+public let rootPath = "SafeKeeper"
+public let appPath = "App"
+public let bundle = "com.sosinvitalii.SafeKeeper"
+public let bundleTest = "com.sosinvitalii.SafeKeeperTests"
+
+// Settings
+public let iOSversion = "16.0"
+public let provisioningProfileApp = "898f6150-92d5-431d-9d5b-5122a773727c"
+public let developmentTeam = "34VDSPZYU9"
+public let codeSignIdentity = "Apple Distribution: Vitalii Sosin (34VDSPZYU9)"
+public let schemesURL = "SafeKeeper"
+
+// API
+public let adMobAppID = " " // TODO: - сделать
+public let identifiersGoogleSKAdNetwork = " " // TODO: - сделать
+public let applinksPageLink = " " // TODO: - сделать
+public let applinksSafeKeeperLink = "SafeKeeper.sosinvitalii.com" // TODO: - сделать
